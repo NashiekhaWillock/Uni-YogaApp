a384384d9b627d276f48f0bc8ba08fcedffaaf9e
Nashiekha Willock <73741861+NashWillock@users.noreply.github.com>
2022-06-29 15:01:36 +0100

    Delete src/assets directory

--- src/assets/images.js
+++
@@ -1,8 +0,0 @@
-export {default as yogaPoses} from './yoga-pose.png';
-export {default as yoga} from './yoga.png';
-export {default as yogaMat} from './yoga-mat.png'
-export {default as file} from './file.png'
-export {default as podium} from './podium_wht.png'
-export {default as logoSm} from './logo_112.png'
-export {default as logoLg} from './logo_lg.png'
-export {default as mindfulness} from './mindfulness.png'
